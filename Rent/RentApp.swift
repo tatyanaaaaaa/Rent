44933962fb26e2fee49afb3730141c0379c46277
Sosina Tatyana <fedorova_t95@mail.ru>
2022-04-16 23:07:36 +0300

    Initial Commit

---
+++ Rent/RentApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentApp.swift
+//  Rent
+//
+//  Created by Tatiana Sosina on 16.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct RentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
